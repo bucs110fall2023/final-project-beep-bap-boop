0ca784193a1981b8da47d4ca9cfa834cadde2f6a
Ryan Wong <rwong11@binghamton.edu>
2023-12-08 22:22:22 -0500

    12=8=23

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import pygame
-#import your controller
+from src.monkeytype_game import MonkeyTypeGame
 
 def main():
+    # Initialize Pygame
     pygame.init()
-    #Create an instance on your controller object
-    #Call your mainloop
-    
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
-# https://codefather.tech/blog/if-name-main-python/
-if __name__ == '__main__':
-    main()
+    monkeytype_game = MonkeyTypeGame()
+    monkeytype_game.run()
+
+main()
